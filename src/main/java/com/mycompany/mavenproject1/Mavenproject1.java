b4f3182f30e09ed762cafde6bfefcf84a35ac7ae
humam-hossain <humam.hossain.e@gmail.com>
2022-05-11 08:43:57 +0600

    updated on Wed, May 11, 2022  8:43:57 AM

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import Source.Login;
+
+/**
+ *
+ * @author humam
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
